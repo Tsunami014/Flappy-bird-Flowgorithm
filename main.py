353d0bf3ef9e31c18d6019792f2d58690167c939
Max Worrall <96847801+Tsunami014@users.noreply.github.com>
2024-03-19 14:03:13 +1100

    Added base files

---
+++ main.py
@@ -0,0 +1,18 @@
+import keyboard
+from time import sleep
+print('Press enter to play! BE CAREFUL, PRESSING ENTER ANYWHERE ELSE WILL BREAK YOUR COMPUTER')
+keyboard.wait('enter')
+while True:
+    if keyboard.is_pressed('q'):
+        keyboard.press_and_release('enter, q, u, i, t, enter')
+        break
+    key = False
+    if keyboard.is_pressed('up') or \
+       keyboard.is_pressed('space') or \
+       keyboard.is_pressed('enter'):
+        
+        key = True
+    if key:
+        keyboard.press_and_release('w')
+    keyboard.press_and_release('enter')
+    sleep(0.1)
